aa1618a958a985e8305020193816321dc7834007
Josh Borrow <josh@joshborrow.com>
2025-04-02 15:35:06 -0400

    Add further to github flow

--- soauth/service/github.py
+++ soauth/service/github.py
@@ -6,30 +6,34 @@ is minted and _all others_ are expired. We may have to revisit
 that point later.
 """
 
-from typing import Any
-from soauth.config.settings import Settings
-from soauth.service.user import read_by_name, UserNotFound
-from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime
+from typing import Any
 
 import httpx
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from soauth.config.settings import Settings
 from soauth.database.user import User
+from soauth.service.user import UserNotFound, read_by_name
 
 
 class GitHubLoginError(Exception):
     pass
 
 
-async def github_api_call(github_api_access_token: str, url: str) -> dict[str, Any]:
+async def github_api_call(
+    github_api_access_token: str | None, url: str
+) -> dict[str, Any]:
     """
     Make an api call to the GitHub APi using your access token.
     """
 
     headers = {
-        "Authorization": f"Bearer {github_api_access_token}",
         "Accept": "application/json",
     }
+
+    if github_api_access_token:
+        headers["Authorization"] = f"Bearer {github_api_access_token}"
 
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers=headers)
@@ -38,6 +42,53 @@ async def github_api_call(github_api_access_token: str, url: str) -> dict[str, A
         raise GitHubLoginError(f"Error contacting {url}")
 
     return response.json()
+
+
+def apply_organization_grants(
+    organization_info: list[dict[str, any]], user: User, settings: Settings
+) -> User:
+    """
+    Checks whether the user has the correct organization grants given a freshly
+    pulled set of organization info from GitHub. If not, we remedy that.
+    """
+
+    # Add grants that have the same name as the GitHub organizations that
+    # we care about.
+    for organization in settings.github_organization_checks:
+        org_found = False
+        for item in organization_info:
+            if item.login == organization:
+                org_found = True
+                user.add_grant(organization)
+                break
+
+        if not org_found:
+            user.remove_grant(organization)
+
+    return user
+
+
+async def github_check_orgs_without_login(
+    user: User, settings: Settings, conn: AsyncSession
+):
+    """
+    Runs the organization check without requiring a login (used for the secondary
+    flow when we do not want to have to re-authenticate with GitHub).
+    """
+
+    organization_info = await github_api_call(
+        None, url=f"https://api.github.com/users/{user.username}/orgs"
+    )
+
+    user = apply_organization_grants(
+        organization_info=organization_info, user=user, settings=settings
+    )
+
+    conn.add(user)
+    await conn.commit()
+    await conn.refresh(user)
+
+    return user
 
 
 async def github_login(code: str, settings: Settings, conn: AsyncSession) -> User:
@@ -108,18 +159,9 @@ async def github_login(code: str, settings: Settings, conn: AsyncSession) -> Use
     user.gh_access_token = gh_access_token
     user.gh_last_logged_in = gh_last_logged_in
 
-    # Add grants that have the same name as the GitHub organizations that
-    # we care about.
-    for organization in settings.github_organization_checks:
-        org_found = False
-        for item in organization_info:
-            if item.login == organization:
-                org_found = True
-                user.add_grant(organization)
-                break
-
-        if not org_found:
-            user.remove_grant(organization)
+    user = apply_organization_grants(
+        organization_info=organization_info, user=user, settings=settings
+    )
 
     conn.add(user)
     await conn.commit()

5f08a0c8ef459a3504f5b33d690bb6afaefe25fc
Josh Borrow <josh@joshborrow.com>
2025-04-08 13:45:34 -0400

    I need those!

--- soauth/core/uuid.py
+++ soauth/core/uuid.py
@@ -1,3 +1,9 @@
 """
 UUID creation. Required because uuid7 was not part of the python standard as of 3.12
 """
+
+from uuid import UUID as UUID
+
+from uuid_extensions import uuid7 as uuid7
+
+__ALL__ = ["UUID", "uuid7"]
